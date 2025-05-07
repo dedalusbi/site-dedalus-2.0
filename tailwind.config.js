bbb95111122ff41fac901d6487f86f53444c6d68
dedalusbi <dedalusbi@gmail.com>
2025-05-07 18:59:58 +0000

    finalização navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,11 @@ module.exports = {
         extend: {
             colors: {
                 'myblue': '#1F487E',
+                'light-green': '#80AE7B',
+                'light-yellow': '#E3BF5D',
+                'light-orange': '#E28E65',
+                'light-blue': '#3D99F0',
+                
             },
             fontFamily: {
                 'sans': ['Creato Display', 'Roboto', 'Arial', 'sans-serif'],  // Example sans-serif font stack
