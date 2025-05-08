64c88b3568098788a32b80279b71c8b867e7fad0
dedalusbi <dedalusbi@gmail.com>
2025-05-08 15:36:26 +0000

    adiciona imagem hero

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,15 @@ module.exports = {
                 'sans': ['Creato Display', 'Roboto', 'Arial', 'sans-serif'],  // Example sans-serif font stack
                 'serif': ['Merriweather', 'serif'],       // Example serif font stack
                 'mono': ['Courier New', 'monospace'],     // Example monospace font stack
+            },
+            borderRadius: {
+                '4xl': '2rem',
+                '5xl': '2.5rem',
+                '6xl': '3rem',
+                '7xl': '3.5rem',
+                '8xl': '4rem',
+                '9xl': '4.5rem',
+                '10xl': '5rem',
             }
         },
     },
